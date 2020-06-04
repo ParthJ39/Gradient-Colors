5fbb1cb4c2e7a3b84a644c1cf05a087affc09ab5
Parthj39 <parth010872@gmail.com>
2020-06-04 16:24:22 +0530

    adding starting project

---
+++ script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("enter");
+
+window.addEventListener('load', function(){
+	css.appendChild(document.createTextNode("rgba(0, 0, 0, 0) linear-gradient(to right, red , yellow) repeat scroll 0% 0%;"));
+})
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+
+button.addEventListener("click", function(){
+	
+	var rr = Math.floor((Math.random() * 255) + 1); 
+	var gg = Math.floor((Math.random() * 255) + 1);
+	var bb = Math.floor((Math.random() * 255) + 1);
+	var rr2 = Math.floor((Math.random() * 255) + 1); 
+	var gg2 = Math.floor((Math.random() * 255) + 1);
+	var bb2 = Math.floor((Math.random() * 255) + 1);
+
+	body.style.background = 
+	"linear-gradient(to right, rgb("+rr+","+gg+","+bb+"), rgb("+rr2+","+gg2+","+bb2+"))";
+	css.textContent = body.style.background + ";";
+
+})
